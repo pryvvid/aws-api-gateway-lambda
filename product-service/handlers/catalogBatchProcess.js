7c96d9ccbe2a34c3f71ec6c8031d505e95510721
pryvvid <pryvvid@gmail.com>
2021-10-04 16:44:00 +0300

    fix: catalogBatchProcess 500 response

--- product-service/handlers/catalogBatchProcess.js
+++ product-service/handlers/catalogBatchProcess.js
@@ -1,35 +1,41 @@
-import AWS from 'aws-sdk';
-import { addProductToDb } from '../utils/addProductToDb';
+import AWS from "aws-sdk";
+import { addProductToDb } from "../utils/addProductToDb";
 
 export const catalogBatchProcess = async (event) => {
   const data = event.Records.map(({ body }) => body);
   const { SNS_TOPIC_ARN } = process.env;
-  const sns = new AWS.SNS({region: 'eu-west-1'});
+  const sns = new AWS.SNS({ region: "eu-west-1" });
 
   for (const product of data) {
     try {
       const addedProduct = await addProductToDb(product);
 
-      await sns.publish({
-        Subject: "New product was added to catalog",
-        Message: JSON.stringify(addedProduct),
-        TopicArn: SNS_TOPIC_ARN,
-        MessageAttributes: {
-          price: {
-            DataType: 'Number',
-            StringValue: `${addedProduct.price}`
-          }
-        },
-      }).promise();
+      await sns
+        .publish({
+          Subject: "New product was added to catalog",
+          Message: JSON.stringify(addedProduct),
+          TopicArn: SNS_TOPIC_ARN,
+          MessageAttributes: {
+            price: {
+              DataType: "Number",
+              StringValue: `${addedProduct.price}`,
+            },
+          },
+        })
+        .promise();
 
       console.log(addedProduct);
     } catch (error) {
       console.error(error);
+      return {
+        statusCode: 500,
+        body: JSON.stringify({ message: "Internal server error" }),
+      };
     }
   }
 
   return {
     statusCode: 202,
+    body: JSON.stringify({ message: "Accepted" }),
   };
-  
 };
--- product-service/test/catalogBatchProcess.test.js
+++ product-service/test/catalogBatchProcess.test.js
@@ -1,34 +1,28 @@
-import AWSMock from 'aws-sdk-mock';
-import { catalogBatchProcess } from '../handlers/catalogBatchProcess';
-import * as addProductToDbFn  from '../utils/addProductToDb'
+import AWSMock from "aws-sdk-mock";
+import { catalogBatchProcess } from "../handlers/catalogBatchProcess";
+import * as addProductToDbFn from "../utils/addProductToDb";
 
 afterEach(() => {
-  AWSMock.restore('SNS');
+  AWSMock.restore("SNS");
 });
 
-test("catalogBatchProcess should send message", async () => {
-
+test("catalogBatchProcess should respond with 202 code", async () => {
   const eventData = {
     Records: [
       {
-        title: 'new game',
-        description: 'new game description',
+        title: "new game",
+        description: "new game description",
         price: 5,
         count: 10,
-        img: 'http://example.com/'
-      }
-    ]
-  }
+        img: "http://example.com/",
+      },
+    ],
+  };
 
-  const spy = jest.spyOn(addProductToDbFn, 'addProductToDb');
+  const spy = jest.spyOn(addProductToDbFn, "addProductToDb");
   spy.mockReturnValue(eventData.Records[0]);
 
-  // AWSMock.mock('S3', 'getSignedUrl', (_action, _params, callback) => {
-  //   console.log('S3', 'getSignedUrl', 'mock called')
-  //   callback(null, mockUrl);
-  // });
-
-  AWSMock.mock('SNS', 'publish', 'test-message');
+  AWSMock.mock("SNS", "publish", "test-message");
 
   const expectedStatusCode = 202;
 
@@ -37,3 +31,29 @@ test("catalogBatchProcess should send message", async () => {
   expect(statusCode).toEqual(expectedStatusCode);
 });
 
+test("catalogBatchProcess should respond with 500 code", async () => {
+  const eventData = {
+    Records: [
+      {
+        title: "new game",
+        description: "new game description",
+        price: 5,
+        count: 10,
+        img: "http://example.com/",
+      },
+    ],
+  };
+
+  const spy = jest.spyOn(addProductToDbFn, "addProductToDb");
+  spy.mockReturnValue(eventData.Records[0]);
+
+  AWSMock.mock("SNS", "publish", () => {
+    throw new Error("Oops");
+  });
+
+  const expectedStatusCode = 500;
+
+  const { statusCode } = await catalogBatchProcess(eventData);
+
+  expect(statusCode).toEqual(expectedStatusCode);
+});
