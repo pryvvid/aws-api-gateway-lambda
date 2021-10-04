9373340df9c8fd3d88c41a71724483b51bdcc8fa
pryvvid <pryvvid@gmail.com>
2021-10-04 21:44:27 +0300

    fix: env.example

--- product-service/test/catalogBatchProcess.test.js
+++ product-service/test/catalogBatchProcess.test.js
@@ -1,26 +1,38 @@
 import AWSMock from "aws-sdk-mock";
 import { catalogBatchProcess } from "../handlers/catalogBatchProcess";
 import * as addProductToDbFn from "../utils/addProductToDb";
+import { addProductToDb } from "../utils/addProductToDb";
+
+const eventData = {
+  Records: [
+    {
+      title: "new game",
+      description: "new game description",
+      price: 5,
+      count: 10,
+      img: "http://example.com/",
+    },
+  ],
+};
 
 afterEach(() => {
   AWSMock.restore("SNS");
 });
 
-test("catalogBatchProcess should respond with 202 code", async () => {
-  const eventData = {
-    Records: [
-      {
-        title: "new game",
-        description: "new game description",
-        price: 5,
-        count: 10,
-        img: "http://example.com/",
-      },
-    ],
-  };
+test('addProductToDb should have been called', async () => {
+  const spyAddProduct = jest.spyOn(addProductToDbFn, "addProductToDb");
+  spyAddProduct.mockReturnValue(eventData.Records[0]);
 
-  const spy = jest.spyOn(addProductToDbFn, "addProductToDb");
-  spy.mockReturnValue(eventData.Records[0]);
+  AWSMock.mock("SNS", "publish", "test-message");
+
+  await catalogBatchProcess(eventData);
+
+  expect(addProductToDb).toHaveBeenCalled()
+})
+
+test("catalogBatchProcess should respond with 202 code", async () => {
+  const spyAddProduct = jest.spyOn(addProductToDbFn, "addProductToDb");
+  spyAddProduct.mockReturnValue(eventData.Records[0]);
 
   AWSMock.mock("SNS", "publish", "test-message");
 
@@ -32,20 +44,8 @@ test("catalogBatchProcess should respond with 202 code", async () => {
 });
 
 test("catalogBatchProcess should respond with 500 code", async () => {
-  const eventData = {
-    Records: [
-      {
-        title: "new game",
-        description: "new game description",
-        price: 5,
-        count: 10,
-        img: "http://example.com/",
-      },
-    ],
-  };
-
-  const spy = jest.spyOn(addProductToDbFn, "addProductToDb");
-  spy.mockReturnValue(eventData.Records[0]);
+  const spyAddProduct = jest.spyOn(addProductToDbFn, "addProductToDb");
+  spyAddProduct.mockReturnValue(eventData.Records[0]);
 
   AWSMock.mock("SNS", "publish", () => {
     throw new Error("Oops");
