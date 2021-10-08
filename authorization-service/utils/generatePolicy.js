705b5e77ad00d78ab67a01d75594095e885e7875
pryvvid <pryvvid@gmail.com>
2021-10-08 16:02:51 +0300

    feat: add authorization service

---
+++ authorization-service/utils/generatePolicy.js
@@ -0,0 +1,15 @@
+export const generatePolicy = (principalId, resource, effect = "Allow") => {
+  return {
+    principalId: principalId,
+    policyDocument: {
+      Version: "2012-10-17",
+      Statement: [
+        {
+          Action: "execute-api:Invoke",
+          Effect: effect,
+          Resource: resource,
+        },
+      ],
+    },
+  };
+};
